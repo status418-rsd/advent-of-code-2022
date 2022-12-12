018e8e0525bc66bf6e0672cf2a6a67a7942bda03
Felix Brock <felix.brock@rockstardevelopers.de>
2022-12-12 21:04:58 +0100

    day 01 | challenge 02

--- advent-01/src/main.rs
+++ advent-01/src/main.rs
@@ -1,25 +1,31 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::cmp;
 
 fn main() {
-    let mut highest: i64 = 0;
-    let mut current_calories: i64 = 0;
-
+    let mut current_calories: i32 = 0;
+    let mut ranking_list =vec![0];
     if let Ok(lines) = read_lines("./input") {
-        for line in lines {
+        let mut peekable = lines.peekable();
+        while let Some(line) = peekable.next() {
             if let Ok(ip) = line {
                 if ip.is_empty() {
-                    highest = cmp::max(current_calories, highest);
+                    ranking_list.push(current_calories);
                     current_calories = 0;
                     continue;
                 }
-                current_calories += ip.parse::<i64>().unwrap();
+                current_calories += ip.parse::<i32>().unwrap();
+                if peekable.peek().is_none() {
+                    ranking_list.push(current_calories);
+                }
             }
         }
     }
-    println!("{}", highest.to_string());
+    ranking_list.sort_by(|a, b| b.cmp(a));
+    let result_1: i32 = ranking_list[0];
+    let result_2: i32 = ranking_list[ .. 3].iter().sum();
+    println!("{}", result_1.to_string());
+    println!("{}", result_2.to_string());
 }
 
 // The output is wrapped in a Result to allow matching on errors
